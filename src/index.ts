e4420ee0771780639080ddc4e3b6cf35f2f4fd39
RaferaX <rafael.larissinha@gmail.com>
2026-09-05 18:42:31 -0300

    Setup inicial: servidor Socket.io com salas por mesa

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { createServer } from "http"
+import { Server } from "socket.io"
+
+const httpServer = createServer()
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("Cliente conectado:", socket.id)
+
+  socket.on("entrarMesa", ({ campaignId, userName }: { campaignId: string; userName: string }) => {
+    socket.data.userName = userName
+    socket.join(campaignId)
+    console.log(`${userName} entrou na mesa ${campaignId}`)
+
+    socket.to(campaignId).emit("jogadorEntrou", { userName })
+  })
+
+  socket.on("sairMesa", (campaignId: string) => {
+    socket.to(campaignId).emit("jogadorSaiu", { userName: socket.data.userName })
+    socket.leave(campaignId)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado:", socket.id)
+  })
+})
+
+const PORT = 4000
+httpServer.listen(PORT, () => {
+  console.log(`Servidor Socket.io rodando na porta ${PORT}`)
+})
